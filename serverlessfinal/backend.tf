e000ffa44c03e2eb8be903fa317a6798c64cb29b
anudeep <97348567+anudeepanu@users.noreply.github.com>
2023-03-15 22:10:53 +0530

    Update backend.tf

--- serverlessfinal/backend.tf
+++ serverlessfinal/backend.tf
@@ -2,8 +2,8 @@ terraform {
   backend "s3" {
     bucket = "backendterraformtest"
     key = "indev-dem-emr"
-    access_key = "AKIATXQ6PBLM6G6OMAFM"
-    secret_key = "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"
+    access_key = "accesskey"
+    secret_key = "secretkey"
     region = "ap-south-1"
   }
 }
